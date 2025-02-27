9e9cc8c4d941314a9719a9a23485973954f51f36
Jonathan Castro <jonathancastro55@gmail.com>
2025-02-27 10:09:02 -0600

    oops

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Spinner } from "@radix-ui/themes";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./components/nav/NavBar";
+import ApplicationViews from "./components/ApplicationViews";
+import { tryGetLoggedInUser } from "./managers/authManager";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loggedInUser, setLoggedInUser] = useState(undefined);
+
+  useEffect(() => {
+    // user will be null if not authenticated
+    tryGetLoggedInUser().then((user) => {
+      setLoggedInUser(user);
+    });
+  }, []);
+
+  // wait to get a definite logged-in state before rendering
+  if (loggedInUser === undefined) {
+    return <Spinner />;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <NavBar loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
+      <div className="container mt-4">
+        <ApplicationViews
+          loggedInUser={loggedInUser}
+          setLoggedInUser={setLoggedInUser}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Theme } from "@radix-ui/themes";
+import App from "./App.jsx";
+import "@radix-ui/themes/styles.css"; // Radix UI styles
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Bootstrap JS
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Theme appearance="light" accentColor="violet" radius="medium">
+      <App />
+    </Theme>
+  </StrictMode>
+);
